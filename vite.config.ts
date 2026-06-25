d733d952730926023c5b38d3d8211f3c69d90f1e
mellowmoonsofttech4u-oss <mellowmoonsofttech4u@gmail.com>
2026-06-25 17:45:51 +0530

    vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import tsconfigPaths from "vite-tsconfig-paths"
 import { tanstackStart } from "@tanstack/react-start/plugin/vite"
-import { nitro } from "nitro/vite"
 
 export default defineConfig({
   plugins: [
-    tanstackStart(),
-    nitro(),
-    react()
+    react(),
+    tsconfigPaths(),
+    tanstackStart()
   ]
 })
